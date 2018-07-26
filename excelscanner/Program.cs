45f32b45f50919c85b4e943e66b8232ae32cf24b
garroad_ran <garroad_ran@hotmail.com>
2018-07-27 08:27:48 +0900

    Initial commit

--- excelscanner/Program.cs
+++ excelscanner/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,38 @@ namespace excelscanner
 {
     class Program
     {
+        static string workingDir = @"C:\Users\owner\Desktop\e"; //Environment.CurrentDirectory
+
         static void Main(string[] args)
         {
+            string[] path = new string[1] { GetDirPath() };
+            FileProcessor fp = new FileProcessor();
+            fp.Process(path);
+
+            Console.ReadLine();
+        }
+
+        static string GetDirPath()
+        {
+            string curpath = workingDir;
+            Console.WriteLine($"Currently working in: {curpath}\nENTER to use current path, or type in a path.");
+
+            while (true)
+            {
+                string retval = Console.ReadLine();
+                if (retval == "")
+                {
+                    return curpath;
+                }
+                else if (Directory.Exists(retval))
+                {
+                    return retval;
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid directory");
+                }
+            }
         }
     }
 }
